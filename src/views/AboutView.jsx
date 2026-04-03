d7baba6d924a48de731b9a7dea059efa38bc862a
Sofia <sofia.montgomery@gmail.com>
2026-04-03 16:24:24 +0200

    Edit AboutView with json text file

--- src/views/AboutView.jsx
+++ src/views/AboutView.jsx
@@ -1,13 +1,17 @@
+import aboutText from "./../texts/about.json";
+
 function AboutView() {
   return (
     <div className="d-flex flex-column justify-content-center align-items-center gap-3 p-4">
-      <h1 className="text-center">About Design machine</h1>
-      <p className="text-center">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt
-        deleniti minima eligendi quia? Cumque culpa fuga libero, ipsam tempore
-        sunt. Saepe autem dolor enim itaque ratione nam error voluptas
-        distinctio.
-      </p>
+      <h2 className="text-center">{aboutText.title}</h2>
+      <div className="p-2 p-sm-5">
+        <h4 className="text-center">{aboutText.introduction}</h4>
+        <div>
+          {aboutText.sections.map((section, index) => (
+            <p key={index}>{section}</p>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
