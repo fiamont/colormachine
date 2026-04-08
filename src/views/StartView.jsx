d45e7dbe7b920409d4fd7d588a7035465636652b
Sofia <sofia.montgomery@gmail.com>
2026-04-08 02:40:21 +0200

    Create GeneratorSection component

--- src/views/StartView.jsx
+++ src/views/StartView.jsx
@@ -1,21 +1,19 @@
+import GeneratorSection from "../components/GeneratorSection";
 import Hero from "../components/hero/Hero";
-import ColorGenerator from "../components/tools/ColorGenerator";
 import ContrastChecker from "../components/tools/ContrastChecker";
-import FontGenerator from "../components/tools/FontGenerator";
-import InspirationGenerator from "../components/tools/InspirationGenerator";
-import PaletteGenerator from "../components/tools/PaletteGenerator";
 
 function StartView() {
   return (
-    <div className="d-flex flex-column justify-content-center align-items-center gap-3">
+    <div className="d-flex flex-column justify-content-center align-items-center">
       <Hero />
-      <section className="d-flex flex-column gap-4 py-3">
-        <p className="text-center m-0">Begin with using a generator</p>
-        <ColorGenerator />
-        <PaletteGenerator />
-        <FontGenerator />
-        <InspirationGenerator />
+      <section className="p-4">
+        <h4>Welcome to Design machine!</h4>
+        <p>
+          Are you in the process of starting a new design project? Need
+          something to start your brainstorming with, or stuck choosing colors?
+        </p>
       </section>
+      <GeneratorSection />
       <section className="d-flex flex-column gap-4 py-3">
         <p className="text-center m-0">Or try any other tool</p>
         <ContrastChecker />
