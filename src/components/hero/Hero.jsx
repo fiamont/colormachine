4f26a1c036d3be75dc94883ce52662fb08dc18ae
Sofia <sofia.montgomery@gmail.com>
2026-04-13 01:24:24 +0200

    Remove background-image add background gradient

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,9 +2,9 @@ import "./Hero.css";
 
 function Hero() {
   return (
-    <div className="hero-container m-0 px-0 py-4 d-flex flex-column justify-content-center align-items-center">
-      <h1 className="text-center hero-heading">Design machine</h1>
-      <h2 className="text-center hero-subheading">
+    <div className="hero-container p-4 d-flex flex-column justify-content-center align-items-end">
+      <h1 className="text-end hero-heading">Design machine</h1>
+      <h2 className="text-end hero-subheading">
         Creative tools for instant design inspiration.
       </h2>
     </div>
