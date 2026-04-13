77bbc5e393f0f7da3431efa19521744ce667fcc2
Sofia <sofia.montgomery@gmail.com>
2026-04-13 03:45:54 +0200

    Add buttons and arrows to StartView

--- src/views/StartView.jsx
+++ src/views/StartView.jsx
@@ -1,22 +1,61 @@
-import GeneratorSection from "../components/GeneratorSection";
 import Hero from "../components/hero/Hero";
-import ContrastChecker from "../components/tools/ContrastChecker";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import arrowLeft from "./../assets/ArrowLeft.png";
+import arrowRight from "./../assets/ArrowRight.png";
 
 function StartView() {
   return (
     <div className="d-flex flex-column justify-content-center align-items-center">
       <Hero />
-      <section className="pt-4 pb-0 px-4">
-        <h4>Welcome to Design machine!</h4>
-        <p>
-          Are you in the process of starting a new design project? Need
-          something to start your brainstorming with, or stuck choosing colors?
+      <section className="w-100 p-3 p-md-5">
+        <p className="ps-1 ps-md-5 fs-4">
+          Are you in the process of starting a new design project?
+        </p>
+        <p className="ps-1 ps-md-5">
+          Need something to start your brainstorming with, or stuck choosing
+          colors?
         </p>
       </section>
-      <GeneratorSection />
-      <section className="d-flex flex-column gap-4 py-3">
-        <p className="text-center m-0">Or try any other tool</p>
-        <ContrastChecker />
+
+      <section className="p-2 px-md-4 pb-5 mb-4 d-flex flex-column w-100 align-items-center">
+        <p className="text-center">
+          Start your designing journey by choosing an option:
+        </p>
+        <div className="pt-0 px-md-4 pb-5 mb-4 d-flex flex-column flex-lg-row w-75">
+          <div className="pt-0 mt-0 ps-md-5 align-self-start">
+            <div className="pb-2">
+              <img src={arrowLeft} alt="arrow" />
+            </div>
+            <div className="p-2">
+              <Button
+                as={Link}
+                to="/tools"
+                variant="light"
+                className="tools-btn"
+                size="lg"
+              >
+                Tools
+              </Button>
+            </div>
+          </div>
+          <div className="pe-md-5 pt-0 mt-0 d-flex flex-column align-self-end">
+            <div className="align-self-end pb-2">
+              <img src={arrowRight} alt="arrow" />
+            </div>
+            <div className="p-2">
+              <Button
+                as={Link}
+                to="/tips"
+                variant="light"
+                className="tips-btn"
+                size="lg"
+              >
+                Tips
+              </Button>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
