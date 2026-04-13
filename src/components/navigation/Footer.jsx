7505ed39bf3fa1454ddec56512b67d17bf244f85
Sofia <sofia.montgomery@gmail.com>
2026-04-13 22:16:31 +0200

    Add tools and tips to footer

--- src/components/navigation/Footer.jsx
+++ src/components/navigation/Footer.jsx
@@ -3,16 +3,22 @@ import "./Navigation.css";
 
 function Footer() {
   return (
-    <div className="d-flex flex-column align-items-center">
-      <div className="d-flex flex-row gap-4">
+    <div className="d-flex flex-column align-items-center bg-light">
+      <div className="d-flex flex-row flex-wrap justify-content-center align-items-center gap-4 py-3 px-5 mx-3 mt-1">
         <Link to="/" className="navigation-link">
-          <p>Home</p>
+          <p className="p-0 m-0">Home</p>
         </Link>
         <Link to="/about" className="navigation-link">
-          <p>About</p>
+          <p className="p-0 m-0">About</p>
         </Link>
         <Link to="/contact" className="navigation-link">
-          <p>Contact</p>
+          <p className="p-0 m-0">Contact</p>
+        </Link>
+        <Link to="/tools" className="navigation-link">
+          <p className="p-0 m-0">Tools</p>
+        </Link>
+        <Link to="/tips" className="navigation-link">
+          <p className="p-0 m-0">Tips</p>
         </Link>
       </div>
       <p>{"\u00A9"} 2026 Design Machine</p>
