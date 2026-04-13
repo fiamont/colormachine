5a7c6f87b8072416d31fb139fb0ebc9f7cf9c5ee
Sofia <sofia.montgomery@gmail.com>
2026-04-13 04:59:23 +0200

    Rename GeneratorSection to ToolSection

--- src/views/ToolView.jsx
+++ src/views/ToolView.jsx
@@ -1,10 +1,10 @@
-import GeneratorSection from "../components/GeneratorSection";
+import ToolSection from "../components/ToolSection";
 
 function ToolView() {
   return (
     <div className="d-flex flex-column justify-content-center align-items-center">
       <h1>Choose your tool</h1>
-      <GeneratorSection />
+      <ToolSection />
     </div>
   );
 }
