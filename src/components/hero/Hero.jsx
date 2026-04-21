d18c7f7232f841214f280001e0706768fa066304
Sofia <sofia.montgomery@gmail.com>
2026-04-21 09:52:12 +0200

    Update name in hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,9 +3,9 @@ import "./Hero.css";
 function Hero() {
   return (
     <div className="hero-container pe-2 pe-md-5 py-0 d-flex flex-column justify-content-start align-items-end">
-      <h1 className="text-end hero-heading">Design machine</h1>
+      <h1 className="text-end hero-heading">Color machine</h1>
       <h2 className="text-end hero-subheading">
-        Tools for faster design decisions.
+        Tools for faster color decisions.
       </h2>
     </div>
   );
