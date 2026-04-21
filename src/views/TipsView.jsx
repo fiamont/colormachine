26df47ceb851df244b31379b852554a9e06c386a
Sofia <sofia.montgomery@gmail.com>
2026-04-21 12:12:19 +0200

    Add basic content to TipsView

--- src/views/TipsView.jsx
+++ src/views/TipsView.jsx
@@ -1,7 +1,49 @@
+import tips from "./../texts/tips.json";
+import ToContact from "../components/about/ToContact";
+import colors from "./../assets/colors.jpg";
+import colorSplash from "./../assets/colorsplash.jpg";
+import colorCorridor from "./../assets/colorcorridor.jpg";
+import StartTeaser from "./../components/about/StartTeaser";
+
 function TipsView() {
   return (
     <div className="d-flex flex-column justify-content-center align-items-center">
-      <h1>Tips here</h1>
+      <h1>{tips.title}</h1>
+      <p>{tips.introduction}</p>
+      <div>
+        <StartTeaser />
+      </div>
+      <div>
+        <div className="d-flex flex-row justify-content-center align-items-center p-2 gap-2">
+          <img
+            src={colors}
+            alt="red blue yellow colors"
+            className="shadow rounded tips-img"
+          />
+          <p>{tips.sections[0]}</p>
+        </div>
+
+        <div className="d-flex flex-row justify-content-center align-items-center p-2 gap-2">
+          <p>{tips.sections[1]}</p>
+          <img
+            src={colorSplash}
+            alt="Splashy colors"
+            className="shadow rounded tips-img"
+          />
+        </div>
+        <div className="d-flex flex-row justify-content-center align-items-center p-2 gap-2">
+          <img
+            src={colorCorridor}
+            alt="rainbow colors in a corridor"
+            className="shadow rounded tips-img"
+          />
+          <p>{tips.sections[2]}</p>
+        </div>
+      </div>
+      <div className="p-4">
+        <p>{tips.contact.text}</p>
+        <ToContact text={tips} />
+      </div>
     </div>
   );
 }
